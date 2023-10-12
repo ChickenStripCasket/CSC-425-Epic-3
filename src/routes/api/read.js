d26e5cb87028ac72af598b4a0c476f937b4ae8d4
clindsey4 <clindsey4@murraystate.edu>
2023-10-12 17:26:25 -0500

    Revert "Add packages required for mongodb."
    This reverts commit b870b3688e369eb6c40171c4f73f1d2a1f6127ba.

--- src/routes/api/read.js
+++ src/routes/api/read.js
@@ -8,8 +8,7 @@ router.get('/task', (req, res, next) => {
     // get the task id from the query (/api/task?id=[number])
     // ?id=[number]
     const taskId = Number(req.query.id)
-
-    // make sure that the task ID is a valid number.
+    // make sure that the task ID is a number.
     if (isNaN(taskId)) {
         return apiError(res, 'Invalid Task ID was provided.', 400)
     }
